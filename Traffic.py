bbbd33d03ec3418cd472442b907ab58c6f2b2207
Sharan Suri <sharansuri@Kapils-MacBook-Air-1141.local>
2020-01-02 12:24:04 +0530

    Added supplementary Code

--- Traffic.py
+++ Traffic.py
@@ -1,10 +1,20 @@
 import cv2
+import socket
 import numpy as np
 from PIL import Image
 from tensorflow.keras import models
 
-#Load the saved model
+#Defining Host and Port
+HOST = 'localhost'
+PORT = '4567'
+
+#Loading lists HIGH and LOW
+HIGH = []
+LOW = []
+
+#Loading the model
 model = models.load_model('/Users/sharansuri/Desktop/IMAGE/keras_model.h5')
+#Initialising The webcam
 video = cv2.VideoCapture(0)
 
 for i in range(100):
@@ -12,19 +22,24 @@ for i in range(100):
         
         im = Image.fromarray(frame, 'RGB')
 
-        
+        #Resizing the array
         im = im.resize((224,224))
+        #Converting into Numpy array again
         img_array = np.array(im)
 
          
         img_array = np.expand_dims(img_array, axis=0)
 
-        
+        #Calling the predict() function 
         prediction = model.predict(img_array)[0][0]
         pper = prediction * 100
+        HIGH.append(pper)
         prediction2 = model.predict(img_array)[0][1]
         pper2 = prediction2 * 100
-        print("HIGH: ",round(pper,2),"%","LOW: ",round(pper2,2),"%")
+        LOW.append(pper2) 
+
+       # Unncomment to see result to each run 
+       # print("HIGH: ",round(pper,2),"%","LOW: ",round(pper2,2),"%")
 
        
 
@@ -34,3 +49,19 @@ for i in range(100):
                 break
 video.release()
 cv2.destroyAllWindows()
+
+#Initialising the variables which would contain the average values
+HIGH_av = 0
+LOW_av = 0
+
+#Calculating the average value for HIGH prediction
+for j in range(0,len(HIGH)):
+    HIGH_av += HIGH[j]
+for k in range(0,len(LOW)):
+    LOW_av += LOW[k]
+
+HIGH_av = HIGH_av/100
+LOW_av = LOW_av/100
+
+#Printing the average values
+print('HIGH%: ',HIGH_av,'LOW%: ',LOW_av)
